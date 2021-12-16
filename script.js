338031cbff1343a91cc40fe7c2a4dd2792ea7d9a
Yuri <korytko_88@mail.ru>
2021-12-16 15:38:17 +0300

    HW_5_Yuri

---
+++ script.js
@@ -0,0 +1,52 @@
+// Задание 1
+/*function subtraction(a, b){
+    return (a - b)/2;
+    }
+    var c = subtraction(20, 8);
+    console.log(c);*/
+
+    // Задание 2
+/*function squareCoube(a, b) {
+  console.log(a ** 2 );
+  console.log(b ** 3 );
+}
+  let result = squareCoube(2, 3)// 4 27
+  //squareCoube(2, 3); // 4 27*/
+    
+// Задание 3
+/*function min(a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+
+  function max(a, b) {
+    if (a > b) {
+      return a;
+    } else {
+      return b;
+    }
+  }*/
+
+
+    // Задание 5
+/*function isEven(number) {
+        
+    if (number % 2 == 0){
+        return true;
+    }else{
+        return false; 
+    }
+    
+}
+      
+var result = isEven(21);
+console.log(result);*/
+
+
+
+  
+    
+    
